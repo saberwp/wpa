a9d851b9c88acceed6286c154852be47946277ca
Casey Milne <casey@saberwp.com>
2023-05-10 20:23:12 +0200

    App localized load copy files.

--- inc/AppManager.php
+++ inc/AppManager.php
@@ -57,8 +57,38 @@ class AppManager {
 	function activate($app_data) {
 
 		var_dump( $app_data );
-		// @TODO copy files from /apps into wp-content/wpa/ 
+		// @TODO copy files from /apps into wp-content/wpa/
 
+		$app_source_path = WPA_PATH.'apps/'.$app_data->local_dir;
+
+		$wp_content_dir = WP_CONTENT_DIR;
+		$wpa_dir = $wp_content_dir . '/wpa';
+
+		$save_path = $wpa_dir . '/' . $app_data->local_dir;
+
+		$this->rcopy($app_source_path, $save_path);
+
+	}
+
+	function rcopy($src, $dst) {
+	  if (file_exists($dst)) $this->rrmdir($dst);
+	  if (is_dir($src)) {
+	    mkdir($dst);
+	    $files = scandir($src);
+	    foreach ($files as $file)
+	    if ($file != "." && $file != "..") $this->rcopy("$src/$file", "$dst/$file");
+	  }
+	  else if (file_exists($src)) copy($src, $dst);
+	}
+
+	function rrmdir($dir) {
+	  if (is_dir($dir)) {
+	    $files = scandir($dir);
+	    foreach ($files as $file)
+	    if ($file != "." && $file != "..") $this->rrmdir("$dir/$file");
+	    rmdir($dir);
+	  }
+	  else if (file_exists($dir)) unlink($dir);
 	}
 
 }
