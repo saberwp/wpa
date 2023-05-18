02b04a91cfae3e0c3eb94e867db1bdf5d6ce23cb
Casey Milne <casey@saberwp.com>
2023-05-18 18:12:44 +0200

    Fix to app management to show all the custom apps defined.

--- inc/Admin.php
+++ inc/Admin.php
@@ -19,6 +19,33 @@ class Admin {
 	        20
 	    );
 
+			add_submenu_page(
+	      'wpa',                  // The slug of the parent menu item
+	      'Dashboard',            // The title of the submenu item
+	      'Dashboard',            // The menu title (shown in the admin menu)
+	      'manage_options',       // The capability required to access the page
+	      'wpa',        // The unique slug for the submenu item
+	      [$this, 'main_content'],   // The callback function to render the page
+		  );
+
+			add_submenu_page(
+	      'wpa',          // The slug of the parent page
+	      'API Keys',     // The title of the submenu page
+	      'API Keys',     // The menu title (shown in the admin menu)
+	      'manage_options',// The capability required to access the page
+	      'wpa-keys', // The unique slug for the submenu page
+	      array( $this, 'keys_page' ) // The callback function to render the page
+		  );
+
+			add_submenu_page(
+	      'wpa',          // The slug of the parent page
+	      'Debug',     // The title of the submenu page
+	      'Debug',     // The menu title (shown in the admin menu)
+	      'manage_options',// The capability required to access the page
+	      'wpa-debug', // The unique slug for the submenu page
+	      array( $this, 'debug_page' ) // The callback function to render the page
+		  );
+
 		});
 
 	}
@@ -28,26 +55,30 @@ class Admin {
 		$am = new AppManager;
 		$available_app_keys = wpa_get_available_app_keys();
 
-		echo '<div id="wpa-admin-container" class="bg-gray-400 text-white wpa-admin-wrap">';
+		echo '<div id="wpa-admin-container" class="-ml-[20px] bg-gray-600 text-white wpa-admin-wrap">';
 
-		// Find apps loaded.
+		// Find apps installed, this includes custom apps created.
 		$wp_content_dir = WP_CONTENT_DIR;
 		$wpa_dir = $wp_content_dir . '/wpa';
-
-		$apps = array_filter( scandir( $wpa_dir ), function( $item ) use ( $wpa_dir ) {
+		$apps_installed = array_filter( scandir( $wpa_dir ), function( $item ) use ( $wpa_dir ) {
 		   return is_dir( $wpa_dir . '/' . $item ) && ! in_array( $item, array( '.', '..' ) );
 		});
 
+		// Merge all prebuilt available apps with current installed apps.
+		$app_list_merged = array_merge($available_app_keys, $apps_installed);
+		$app_list        = array_unique($app_list_merged);
+
+
 		$app_defs_with_install_flag = [];
-		foreach( $available_app_keys as $available_app_key ) {
+		foreach( $app_list as $app_key ) {
 
 			// Check if installed, then load app def from install if it is.
-			$installed = wpa_app_installed($available_app_key);
+			$installed = wpa_app_installed($app_key);
 
 			if($installed) {
-				$app_def = wpa_load_app_def($available_app_key);
+				$app_def = wpa_load_app_def($app_key);
 			} else {
-				$app_def = wpa_load_app_def_available($available_app_key);
+				$app_def = wpa_load_app_def_available($app_key);
 			}
 
 			$app_def->installed = $installed;
@@ -65,6 +96,16 @@ class Admin {
 
 	}
 
+	public function keys_page() {
 
+		echo 'KEYS PAGE';
+
+	}
+
+	public function debug_page() {
+
+		echo 'DEBUG PAGE';
+
+	}
 
 }
--- js/admin.js
+++ js/admin.js
@@ -12,14 +12,14 @@ class WpaAdmin {
 
 	makeAppHeading() {
 		const el = document.createElement('h2')
-		el.classList.add('font-bold', 'text-2xl', 'text-white')
+		el.classList.add('py-2', 'px-4', 'font-bold', 'text-2xl', 'text-white')
 		el.innerHTML = 'APP LIST'
 		this.container.appendChild(el)
 	}
 
 	makeAppGrid() {
 		const el = document.createElement('section')
-		el.classList.add('flex', 'flex-wrap', 'gap-4', 'bg-gray-800', 'p-4')
+		el.classList.add('flex', 'flex-wrap', 'gap-4', 'bg-gray-100', 'p-4')
 		this.data.forEach((app) => {
 			el.appendChild(this.makeAppCard(app))
 		})
@@ -29,16 +29,16 @@ class WpaAdmin {
 	makeAppCard(app) {
 		const el = document.createElement('div')
 		el.id = 'wpa-app-'+app.key
-		el.classList.add('rounded-md', 'p-4', 'bg-gray-600')
-		el.appendChild(this.makeAppCardHeader(app.title))
+		el.classList.add('rounded-md', 'p-4', 'bg-gray-600', 'w-60', 'h-40', 'flex', 'flex-col')
+		el.appendChild(this.makeAppCardHeader(app))
 		el.appendChild(this.makeAppCardBody(app))
 		el.appendChild(this.makeAppCardFooter(app))
 		return el
 	}
 
-	makeAppCardHeader(appTitle) {
+	makeAppCardHeader(app) {
 		const el = document.createElement('header')
-		el.innerHTML = '<h2 class="text-white text-xl font-bold mb-6">'+appTitle+'</h2>'
+		el.innerHTML = '<h2 class="text-white text-xl font-semibold mb-6">'+app.title+'</h2>'
 		return el
 	}
 
@@ -57,9 +57,9 @@ class WpaAdmin {
 
 	makeAppCardFooter(app) {
 		const el = document.createElement('footer')
-		el.classList.add('flex', 'gap-4', 'items-center')
+		el.classList.add('flex', 'gap-4', 'items-center', 'justify-between', 'mt-auto')
 
-
+		el.appendChild(this.makeAppCardRemoveButton(app))
 		el.appendChild(this.makeAppCardStatus(app))
 		return el
 	}
@@ -68,8 +68,28 @@ class WpaAdmin {
 		const el = document.createElement('div')
 		el.classList.add('cursor-pointer','wpa-app-install-button')
 		let content = ''
-		content += '<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3125 1.3125V0H12.6875V1.3125V16.0836L7.49219 10.8883L6.5625 9.95859L4.70859 11.8125L5.63828 12.7422L13.0703 20.1797L14 21.1094L14.9297 20.1797L22.3672 12.7422L23.2969 11.8125L21.4375 9.95859L20.5078 10.8883L15.3125 16.0836V1.3125ZM7.04375 16.625H2.625H0V19.25V25.375V28H2.625H25.375H28V25.375V19.25V16.625H25.375H20.9563L18.3313 19.25H25.375V25.375H2.625V19.25H9.66875L7.04375 16.625ZM23.625 22.3125C23.625 21.9644 23.4867 21.6306 23.2406 21.3844C22.9944 21.1383 22.6606 21 22.3125 21C21.9644 21 21.6306 21.1383 21.3844 21.3844C21.1383 21.6306 21 21.9644 21 22.3125C21 22.6606 21.1383 22.9944 21.3844 23.2406C21.6306 23.4867 21.9644 23.625 22.3125 23.625C22.6606 23.625 22.9944 23.4867 23.2406 23.2406C23.4867 22.9944 23.625 22.6606 23.625 22.3125Z" fill="white"/></svg>'
+		content += '<svg class="fill-white hover:fill-gray-200" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3125 1.3125V0H12.6875V1.3125V16.0836L7.49219 10.8883L6.5625 9.95859L4.70859 11.8125L5.63828 12.7422L13.0703 20.1797L14 21.1094L14.9297 20.1797L22.3672 12.7422L23.2969 11.8125L21.4375 9.95859L20.5078 10.8883L15.3125 16.0836V1.3125ZM7.04375 16.625H2.625H0V19.25V25.375V28H2.625H25.375H28V25.375V19.25V16.625H25.375H20.9563L18.3313 19.25H25.375V25.375H2.625V19.25H9.66875L7.04375 16.625ZM23.625 22.3125C23.625 21.9644 23.4867 21.6306 23.2406 21.3844C22.9944 21.1383 22.6606 21 22.3125 21C21.9644 21 21.6306 21.1383 21.3844 21.3844C21.1383 21.6306 21 21.9644 21 22.3125C21 22.6606 21.1383 22.9944 21.3844 23.2406C21.6306 23.4867 21.9644 23.625 22.3125 23.625C22.6606 23.625 22.9944 23.4867 23.2406 23.2406C23.4867 22.9944 23.625 22.6606 23.625 22.3125Z"/></svg>'
 
+		el.innerHTML = content
+		el.setAttribute('app-key',app.key)
+		return el
+	}
+
+	makeAppCardRemoveButton(app) {
+		let iconOpacity = .15
+		if(app.installed) {
+			iconOpacity = .5
+		}
+		const el = document.createElement('div')
+
+		let content = ''
+		if(app.installed) {
+			el.classList.add('cursor-pointer','wpa-app-remove-button')
+			content += '<svg class="fill-gray-400 hover:fill-gray-200" width="15" height="17" viewBox="0 0 15 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.35714 0H4.92857L4.6875 0.355273L3.14062 2.65625H2.67857H1.07143H0V4.25H1.07143V15.4062V17H2.67857H12.3214H13.9286V15.4062V4.25H15V2.65625H13.9286H12.3214H11.8594L10.3125 0.355273L10.0714 0H9.64286H5.35714ZM9.92745 2.65625H5.07254L5.78571 1.59375H9.21429L9.92745 2.65625ZM2.67857 15.4062V4.25H12.3214V15.4062H2.67857ZM10.7779 7.4375L9.64286 6.31191L9.07366 6.87637L7.5 8.43691L5.92634 6.87637L5.35714 6.31191L4.2221 7.4375L4.79129 8.00195L6.36496 9.5625L4.79129 11.123L4.2221 11.6875L5.35714 12.8131L5.92634 12.2486L7.5 10.6881L9.07366 12.2486L9.64286 12.8131L10.7779 11.6875L10.2087 11.123L8.63504 9.5625L10.2087 8.00195L10.7779 7.4375Z"/></svg>'
+		}
+		if(!app.installed) {
+			content += '<svg width="15" height="17" viewBox="0 0 15 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.35714 0H4.92857L4.6875 0.355273L3.14062 2.65625H2.67857H1.07143H0V4.25H1.07143V15.4062V17H2.67857H12.3214H13.9286V15.4062V4.25H15V2.65625H13.9286H12.3214H11.8594L10.3125 0.355273L10.0714 0H9.64286H5.35714ZM9.92745 2.65625H5.07254L5.78571 1.59375H9.21429L9.92745 2.65625ZM2.67857 15.4062V4.25H12.3214V15.4062H2.67857ZM10.7779 7.4375L9.64286 6.31191L9.07366 6.87637L7.5 8.43691L5.92634 6.87637L5.35714 6.31191L4.2221 7.4375L4.79129 8.00195L6.36496 9.5625L4.79129 11.123L4.2221 11.6875L5.35714 12.8131L5.92634 12.2486L7.5 10.6881L9.07366 12.2486L9.64286 12.8131L10.7779 11.6875L10.2087 11.123L8.63504 9.5625L10.2087 8.00195L10.7779 7.4375Z" fill="white" fill-opacity="'+iconOpacity+'"/></svg>'
+		}
 		el.innerHTML = content
 		el.setAttribute('app-key',app.key)
 		return el
@@ -79,7 +99,7 @@ class WpaAdmin {
 		const el = document.createElement('div')
 		el.classList.add('cursor-pointer','wpa-app-refresh-button')
 		let content = ''
-		content += '<svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28 11.2H26.6H18.2H16.8V8.4H18.2H23.2225L20.1833 5.36083C18.5383 3.72167 16.3158 2.8 14 2.8C9.75917 2.8 6.11333 5.3725 4.55 9.0475L1.97167 7.95083C3.96083 3.27833 8.59833 0 14 0C17.0625 0 19.9967 1.21333 22.1608 3.3775L25.2 6.4225V1.4V0H28V1.4V9.8V11.2ZM0 14.9333H1.4H9.8H11.2V17.7333H9.8H4.7775L7.81667 20.7725C9.46167 22.4117 11.6842 23.3333 14 23.3333C18.235 23.3333 21.875 20.7725 23.4442 17.1033L26.0167 18.2058C24.0217 22.8667 19.3958 26.1333 14 26.1333C10.9375 26.1333 8.00333 24.92 5.83917 22.7558L2.8 19.7108V24.7333V26.1333H0V24.7333V16.3333V14.9333Z" fill="white"/></svg>'
+		content += '<svg class="fill-white hover:fill-gray-200" width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28 11.2H26.6H18.2H16.8V8.4H18.2H23.2225L20.1833 5.36083C18.5383 3.72167 16.3158 2.8 14 2.8C9.75917 2.8 6.11333 5.3725 4.55 9.0475L1.97167 7.95083C3.96083 3.27833 8.59833 0 14 0C17.0625 0 19.9967 1.21333 22.1608 3.3775L25.2 6.4225V1.4V0H28V1.4V9.8V11.2ZM0 14.9333H1.4H9.8H11.2V17.7333H9.8H4.7775L7.81667 20.7725C9.46167 22.4117 11.6842 23.3333 14 23.3333C18.235 23.3333 21.875 20.7725 23.4442 17.1033L26.0167 18.2058C24.0217 22.8667 19.3958 26.1333 14 26.1333C10.9375 26.1333 8.00333 24.92 5.83917 22.7558L2.8 19.7108V24.7333V26.1333H0V24.7333V16.3333V14.9333Z"/></svg>'
 
 		el.innerHTML = content
 		el.setAttribute('app-key',app.key)
@@ -90,11 +110,16 @@ class WpaAdmin {
 
 	makeAppCardVisitButton(app) {
 		const el = document.createElement('div')
-		el.classList.add('cursor-pointer')
 		let content = ''
-		content += '<a href="'+app.site_url+'">'
-		content += '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.0594 2.55938L9 1.5L10.0594 0.440625L10.5 0H14.5H16V1.5V5.5L15.5594 5.94063L14.5 7L13.4406 5.94063L12.2812 4.78125L7.53125 9.53125L7 10.0625L5.94063 9L6.47188 8.46875L11.2188 3.71875L10.0594 2.55938ZM14.5 4.87813V1.5H11.1219L14.5 4.87813ZM0 1H0.75H6.25H7V2.5H6.25H1.5V14.5H13.5V9.75V9H15V9.75V15.25V16H14.25H0.75H0V15.25V1.75V1Z" fill="black"/></svg>'
-		content += '</a>'
+		if(app.installed) {
+			el.classList.add('cursor-pointer')
+			content += '<a href="'+app.site_url+'">'
+			content += '<svg class="fill-white hover:fill-gray-200" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.5742 3.19922L11.25 1.875L12.5742 0.550781L13.125 0H18.125H20V1.875V6.875L19.4492 7.42578L18.125 8.75L16.8008 7.42578L15.3516 5.97656L9.41406 11.9141L8.75 12.5781L7.42578 11.25L8.08984 10.5859L14.0234 4.64844L12.5742 3.19922ZM18.125 6.09766V1.875H13.9023L18.125 6.09766ZM0 1.25H0.9375H7.8125H8.75V3.125H7.8125H1.875V18.125H16.875V12.1875V11.25H18.75V12.1875V19.0625V20H17.8125H0.9375H0V19.0625V2.1875V1.25Z"/></svg>'
+			content += '</a>'
+		}
+		if(!app.installed) {
+			content += '<svg class="fill-gray-500" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.5742 3.19922L11.25 1.875L12.5742 0.550781L13.125 0H18.125H20V1.875V6.875L19.4492 7.42578L18.125 8.75L16.8008 7.42578L15.3516 5.97656L9.41406 11.9141L8.75 12.5781L7.42578 11.25L8.08984 10.5859L14.0234 4.64844L12.5742 3.19922ZM18.125 6.09766V1.875H13.9023L18.125 6.09766ZM0 1.25H0.9375H7.8125H8.75V3.125H7.8125H1.875V18.125H16.875V12.1875V11.25H18.75V12.1875V19.0625V20H17.8125H0.9375H0V19.0625V2.1875V1.25Z"/></svg>'
+		}
 		el.innerHTML = content
 		return el
 	}
@@ -104,7 +129,7 @@ class WpaAdmin {
 		const el = document.createElement('div')
 		el.classList.add('wpa-app-card-status')
 		let content = ''
-		content += '<h2 class="text-gray-200 text-lg font-semibold">'+label+'</h2>'
+		content += '<h2 class="text-gray-400 text-base font-medium">'+label+'</h2>'
 		el.innerHTML = content
 		return el
 	}
