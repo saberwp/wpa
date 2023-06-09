596f35d0af24ed4d96e8f4d2968c5fc04ad3f731
Casey Milne <casey@saberwp.com>
2023-06-09 11:52:51 +0200

    Refactor move create callback from Api to ApiCreate.

--- inc/ApiCreate.php
+++ inc/ApiCreate.php
@@ -90,4 +90,51 @@ class ApiCreate {
 		$this->insert_data[$this->model_def->relations->right->model.'_id'] = $this->form_data[$this->model_def->relations->right->model.'_id'];
 	}
 
+	public static function create_callback($request) {
+
+		// Get model key from API path.
+	  $route = $request->get_route();
+	  $route_parts = explode('/', $route);
+		$app_key = $route_parts[3];
+	  $model_key = $route_parts[4];
+
+		// Init the ApiCreate class.
+		$api_create = new ApiCreate();
+		$api_create->app_key = $app_key;
+		$api_create->model_key = $model_key;
+		$api_create->form_data = $request->get_json_params();
+		$api_create->table_name = Api::make_table_name($app_key, $model_key);
+		$api_create->init();
+		$api_create->parse_form_data();
+		$api_create->db_insert();
+
+	  if ($api_create->insert_result === false) {
+	    // Handle insert error.
+	    $response = new \stdClass;
+	    $response->success = false;
+	    $response->message = 'Error processing insert request.';
+	  } else {
+	    // Handle insert success.
+	    $response = new \stdClass;
+	    $response->success = true;
+	    $response->message = 'Request processed successfully.';
+			$response->insert_id = $api_create->insert_id;
+			$response->model_key = $api_create->model_def->key;
+	  }
+
+		// If relations exist, do relations handling.
+		if( $api_create->relations_exist === 1 ) {
+			$api_relations = new ApiRelations;
+			$api_relations->app_key = $api_create->app_key;
+			$api_relations->model_def = $api_create->model_def;
+			$api_relations->record_id = $api_create->insert_id;
+			$api_relations->storage_path = $api_create->storage_path;
+			$api_relations->form_data = $api_create->form_data;
+			$response->relations = $api_relations->process();
+		}
+
+		return rest_ensure_response($response);
+
+	}
+
 }
