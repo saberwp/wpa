1bdd2a54c5c986e2d7e5e61a020f856003688715
Casey Milne <casey@saberwp.com>
2023-06-19 12:28:06 +0300

    Remove old logo component, update footer.

--- render/components/Footer.js
+++ render/components/Footer.js
@@ -4,9 +4,13 @@ class Footer {
 		const el = document.createElement('footer')
 		el.id = 'app-footer'
 		el.classList.add('flex', 'text-white', 'py-2', 'gap-4', 'justify-center', 'items-center')
-		const logo = new Logo()
-		logo.setIconWidth('w-10')
-		el.appendChild( logo.render() ) // Add nested logo el.
+
+		// Footer Logo
+		const iconLogo = new IconLogo
+		iconLogo.setSvgMarkup(app.def.logo)
+		iconLogo.build()
+		el.appendChild(iconLogo.get())
+
 		return el
 	}
 
